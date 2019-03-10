8c5135f030501b87cbe8441f6466928c23ac4fe7
Simon <simon.larsson@protonmail.com>
2019-03-10 17:40:12 +0100

    Release

---
+++ src/buffer.rs
@@ -0,0 +1,113 @@
+use std::collections::HashMap;
+use std::default::Default;
+use std::io::{BufRead, BufReader};
+use std::iter;
+
+use discord::{
+    model::{ChannelId, MessageId},
+    Discord,
+};
+use std::sync::{mpsc::Sender, Arc, Mutex};
+
+pub struct ShellBuffer {
+    discord: Discord,
+    pub ttys: HashMap<u8, Option<TTY>>,
+}
+
+impl ShellBuffer {
+    pub fn new() -> Self {
+        let mut ttys = HashMap::new();
+        for i in 0..10 as u8 {
+            ttys.insert(i, None);
+        }
+
+        let dg =
+            Discord::from_bot_token(&std::env::var("DISCORD_TOKEN").expect("No discord token"))
+                .unwrap();
+
+        ShellBuffer {
+            discord: dg,
+            ttys: ttys,
+        }
+    }
+
+    fn get_tty(&mut self, n: u8) -> &mut Option<TTY> {
+        self.ttys.get_mut(&n).unwrap()
+    }
+
+    pub fn write_to(&mut self, tty_num: u8, line: &str, channel_id: ChannelId) {
+        match self.get_tty(tty_num) {
+            Some(tty) => {
+                tty.write(line);
+            }
+            None => {
+                // Doesn't exist, lets create it
+                let m = self
+                    .discord
+                    .send_message(channel_id, &format!("**New TTY:** {}", tty_num), "", false)
+                    .expect(
+                        "Could not send message response, breaking here to prevent recursion leak",
+                    );
+                let new = TTY::new(m.channel_id, m.id);
+                self.set_tty(tty_num, Some(new));
+                self.write_to(tty_num, line, channel_id);
+            }
+        };
+    }
+
+    pub fn set_tty(&mut self, n: u8, tty: Option<TTY>) {
+        let old = self.ttys.get_mut(&n).unwrap();
+        *old = tty;
+    }
+
+    pub fn display(&mut self, tty_num: u8) {
+        let tty = match self.get_tty(tty_num) {
+            Some(tty) => tty,
+            None => {
+                eprintln!("TTY not open: {}", tty_num);
+                return;
+            }
+        };
+        let (cid, mid) = (tty.channel_id, tty.message_id);
+        let mut content = tty.text.clone().to_vec();
+
+        content[0] = format!("**TTY {}**\n```\n", tty_num);
+        content.push("\n```".to_owned());
+
+        self.discord
+            .edit_message(cid, mid, &content.join("\n"))
+            .unwrap();
+    }
+}
+
+pub struct TTY {
+    last_command: String,
+    channel_id: ChannelId,
+    message_id: MessageId,
+    text: [String; 20],
+}
+
+impl TTY {
+    fn new(channel_id: ChannelId, message_id: MessageId) -> Self {
+        let mut empties: [String; 20] = std::default::Default::default();
+        for i in 0..empties.len() {
+            empties[i] = "-> ".to_owned();
+        }
+        TTY {
+            last_command: "**Spawning new TTY...**".to_owned(),
+            channel_id: channel_id,
+            message_id: message_id,
+            text: empties,
+        }
+    }
+
+    fn write(&mut self, line: &str) {
+        for i in 0..self.text.len() {
+            if i == 0 {
+                continue;
+            }
+            self.text[i - 1] = self.text[i].clone();
+        }
+        (self.text[19] = format!("-> {}", line));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,128 @@
+extern crate discord;
+
+mod buffer;
+use buffer::ShellBuffer;
+
+use discord::model::{ChannelId, Event, Message, MessageId, UserId};
+use discord::Discord;
+
+use std::env::var;
+use std::io::{BufRead, BufReader};
+use std::process::Command as bash;
+use std::process::Stdio;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+
+fn main() {
+    // Log in to Discord using a bot token from the environment
+    let discord = Discord::from_bot_token(&var("DISCORD_TOKEN").expect("No discord token"))
+        .expect("login failed");
+
+    // Establish and use a websocket connection
+    let (mut connection, _) = discord.connect().expect("connect failed");
+    println!("Successfully connected");
+
+    let shell_session = Arc::new(Mutex::new(ShellBuffer::new()));
+
+    loop {
+        match connection.recv_event() {
+            Ok(Event::MessageCreate(m)) => {
+                let command = {
+                    if m.content.chars().next().or(Some('.')).unwrap() == '$' {
+                        if m.content.len() < 4 {
+                            continue;
+                        };
+                        if m.author.id != UserId(220120722939445248)
+                            && m.author.id != UserId(181200309949825024)
+                        {
+                            discord.send_message(
+                                m.channel_id,
+                                "Ni andra får använda detta när jag sattit upp det i en VM",
+                                "",
+                                false,
+                            );
+                            continue;
+                        }
+                        let bytes: Vec<char> = m.content.trim().chars().collect();
+                        let tty: u8 = match bytes[1].to_string().parse() {
+                            Ok(n) => n,
+                            Err(_) => {
+                                discord
+                                    .send_message(
+                                        m.channel_id,
+                                        "Parse error: no tty specified",
+                                        "",
+                                        false,
+                                    )
+                                    .unwrap();
+                                continue;
+                            }
+                        };
+                        if !(bytes[2] == '(' && *bytes.last().unwrap() == ')') {
+                            Command::Invalid(
+                                "Parse error: invalid parenthesis, syntax is $`tty_number`(`shell_format`)".to_owned(),
+                            )
+                        } else {
+                            Command::Shell(
+                                bytes[3..bytes.len() - 1]
+                                    .iter()
+                                    .cloned()
+                                    .collect::<String>(),
+                                tty,
+                            )
+                        }
+                    } else {
+                        Command::Invalid("".to_owned())
+                    }
+                };
+                println!("a");
+
+                act(&discord, m, command, shell_session.clone());
+            }
+            Ok(_) => {}
+            Err(discord::Error::Closed(code, body)) => {
+                println!("Gateway closed on us with code {:?}: {}", code, body);
+                break;
+            }
+            Err(err) => println!("Receive error: {:?}", err),
+        };
+    }
+}
+
+enum Command {
+    Shell(String, u8),
+    Invalid(String),
+}
+
+fn act(dg: &Discord, m: Message, command: Command, shell_session: Arc<Mutex<ShellBuffer>>) {
+    match command {
+        Command::Shell(command, tty_num) => {
+            let com = bash::new("sh")
+                .arg("-c")
+                .arg(command)
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap()
+                .stdout
+                .ok_or_else(|| {
+                    "Could not capture standard output";
+                    return;
+                })
+                .unwrap();
+
+            thread::spawn(move || {
+                let reader = BufReader::new(com);
+                reader.lines().for_each(|line| {
+                    let mut sh = shell_session.lock().unwrap();
+                    sh.write_to(tty_num, &line.unwrap(), m.channel_id);
+                    sh.display(tty_num);
+                });
+            });
+        }
+        Command::Invalid(text) => {
+            if !text.is_empty() {
+                dg.send_message(m.channel_id, &text, "", false).unwrap();
+            }
+        }
+    }
+}
